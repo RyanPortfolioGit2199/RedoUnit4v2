3a1d12ea1b028edaa32feae1681e2bbce1d64185
RyanPortfolioGit2199 <ryan.allen.2199@gmail.com>
2025-04-13 19:10:04 -0700

    finished with the project

--- Assets/Course Library/Scripts/Enemy.cs
+++ Assets/Course Library/Scripts/Enemy.cs
@@ -17,9 +17,15 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 lookDirection = player.transform.position - transform.position;
-        // Add force that pushes the enemy to the location of the player.
-        // .normalized makes it so that the enemy will always move to towards the player at the same speed no matter the distance, ie: The farther away the player is from the player the faster the enemy with go to reach the player. The closer the enemy is to the player the slower the enemy will go to reach the player.
-        enemyRb.AddForce((lookDirection).normalized * speed);
+        Vector3 lookDirection = player.transform.position - transform.position;// Add force that pushes the enemy to the location of the player.
+      
+        enemyRb.AddForce((lookDirection).normalized * speed);// .normalized makes it so that the enemy will always move to towards the player at the same speed no matter the distance, ie: The farther away the player is from the player the faster the enemy with go to reach the player. The closer the enemy is to the player the slower the enemy will go to reach the player.
+
+        if (transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
+
+
     }
 }
--- Assets/Course Library/Scripts/SpawnManager.cs
+++ Assets/Course Library/Scripts/SpawnManager.cs
@@ -4,19 +4,34 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
     private float spawnRange = 9;
+    public int enemyCount;
+    public int waveNumber = 1;
+    public GameObject powerupPrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-
-        // Spawns an enemy based on the enemy in the prefab folder at random x and z coordinates generated in the GenerateSpawnPosition method below
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsByType<Enemy>(FindObjectsSortMode.None).Length; //Find objects all objects in the scene with the Enemy Script, SortMode has the amount of enemies stored in an array, the .None mean we dont need to sort them. But since the amount of enemies is stored in an array, the .Length is used to covert the array of enemies into a integer.
+        if (enemyCount == 0)
+        {
+            waveNumber++; //Increases the amount of the integer varable waveNumber everytime enemyCount equals 0.
+            SpawnEnemyWave(waveNumber); // if the enemy count equals 0 run the SpawnEnemyWave method to spawn the amount of enemies based on the integer variable waveNumber.
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation); // when the enemyCount equals 0 spawn a powerup at coordinates reusing GenerateSpawnPosition method.
+        }
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++) // this for loops spawns a certain number of enemies based on the integer variable enemiesToSpawn, it starts at 0, every time an enemy is spawned i increases until i equals the integer variable enemiesToSpawn.
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation); // Spawns an enemy based on the enemy in the prefab folder at random x and z coordinates generated in the GenerateSpawnPosition method below
+        }
     }
 
     private Vector3 GenerateSpawnPosition()
